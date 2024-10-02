92ae5e7f7b9e31094ead6bd071a9fa11b0cedcab
Asim Rasheed <asimrasheed442@gmail.com>
2024-10-02 08:25:46 +0500

    card

--- src/app/(customer)/page.jsx
+++ src/app/(customer)/page.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import "@/style/home.scss";
 import s_banner from "@/public/s_banner.png";
 import banner from "@/public/banner_1.jpg";
+// import banner_2 from "@/public/banner_2.jpg";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -41,6 +42,29 @@ export default function page() {
           <Image className="page__banner__img" src={banner} alt="food" />
         </SwiperSlide>
       </Swiper>
+      <div className="product__container">
+        <div className="product__main__text__title">Most Popular Food</div>
+
+        <div className="product__main__cards">
+          <div className="product__main__card">
+            <div className="product__main__card__img">
+              <Image
+                className="product__main__card__img__image"
+                src={banner}
+                alt="fast food"
+              />
+            </div>
+            <div className="product__main__card__title">Family Plater </div>
+            <div className="product__main__card__sub__title">
+              1 whole Chicken, 5 Wings
+            </div>
+            <div className="product__main__card__price__warper">
+              <div className="products__price">$ 50.20</div>
+              <div className="products__add">+</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
