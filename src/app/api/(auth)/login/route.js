0f02bd8c111f17e0e1ba3da391bf9357a6219765
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-13 17:04:57 +0500

    Create route.js

---
+++ src/app/api/(auth)/login/route.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { cookies } from "next/headers";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+    }
+
+    const exist = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        password: true,
+        id: true,
+      },
+    });
+
+    if (!exist) {
+      return NextResponse.json({ message: "User not found" }, { status: 400 });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, exist.password);
+
+    if (!isPasswordCorrect) {
+      return NextResponse.json(
+        { message: "Incorrect password" },
+        { status: 400 }
+      );
+    }
+
+    cookies().set("token", exist.id, {
+      maxAge: 60 * 60 * 24 * 365,
+    });
+
+    return NextResponse.json({ message: "Login successful" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Internal Error" }, { status: 500 });
+  }
+}
