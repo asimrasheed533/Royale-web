c954e07eb4ac4674610e01fb5860f82ecedc37e3
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-13 17:22:39 +0500

    Update layout.js

--- src/app/(customer)/layout.js
+++ src/app/(customer)/layout.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Layout from "@/components/layout";
-export default function layout({ children }) {
-  return (
-    <div>
-      <Layout>{children}</Layout>
-    </div>
-  );
+import { cookies } from "next/headers";
+
+export default async function layout({ children }) {
+  const token = cookies().get("token")?.value || null;
+
+  return <Layout token={token}>{children}</Layout>;
 }
