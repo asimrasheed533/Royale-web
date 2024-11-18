4a7206361600f99b4be8efc22f89429a5c7a02ce
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-18 11:16:02 +0500

    Update headerItem.jsx

--- src/constant/headerItem.jsx
+++ src/constant/headerItem.jsx
@@ -3,7 +3,7 @@ export default {
     { key: "id", name: "id" },
     { key: "name", name: "product Name" },
     { key: "city", name: "City" },
-    { key: "logo", name: "Logo" },
+    // { key: "logo", name: "Logo", style: { width: "200px" } },
     { key: "status", name: "Status" },
     { key: "quantity", name: "Quantity" },
     { key: "totalPrice", name: "Total Price" },
