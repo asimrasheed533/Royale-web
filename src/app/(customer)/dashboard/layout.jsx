2e03ed73b8e7b11aff21412221ea062ca428b7c0
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-18 12:13:12 +0500

    Update layout.jsx

--- src/app/(customer)/dashboard/layout.jsx
+++ src/app/(customer)/dashboard/layout.jsx
@@ -1,12 +1,16 @@
 import SideBar from "@/components/SideBar";
 import React from "react";
 import "@/style/listing.scss";
+import DashboardHeader from "@/components/DashboardHeader";
 export default function layout({ children }) {
   return (
     <div>
       <div className="dashboard">
         <SideBar />
-        <div className="dashboard__main__content">{children}</div>
+        <div className="dashboard__main__content">
+          <DashboardHeader />
+          {children}
+        </div>
       </div>
     </div>
   );
