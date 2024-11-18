73b477ec0a88cc835ac23f7e81bd8f1531b76e24
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-18 11:49:22 +0500

    Update headerItem.jsx

--- src/constant/headerItem.jsx
+++ src/constant/headerItem.jsx
@@ -1,9 +1,11 @@
 export default {
   order: [
     { key: "id", name: "id" },
-    { key: "name", name: "product Name" },
+    {
+      key: "name",
+      name: "product Name",
+    },
     { key: "city", name: "City" },
-    // { key: "logo", name: "Logo", style: { width: "200px" } },
     { key: "status", name: "Status" },
     { key: "quantity", name: "Quantity" },
     { key: "totalPrice", name: "Total Price" },
