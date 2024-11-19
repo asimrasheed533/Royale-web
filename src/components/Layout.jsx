bda81fa5d939d1344b7827569b630d11f6ea7b19
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-19 19:51:22 +0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const ROUTES_NO_HEADER_FOOTER = ["/signIn", "/signUp", "/dashboard"];
 
 export default function Layout({ children, token }) {
   const pathname = usePathname();
-
-  // const shouldShowHeaderFooter = !ROUTES_NO_HEADER_FOOTER.includes(pathname);
 
   const shouldShowHeaderFooter = !ROUTES_NO_HEADER_FOOTER.some((route) =>
     pathname.startsWith(route)
@@ -18,6 +18,7 @@ export default function Layout({ children, token }) {
     <div>
       {shouldShowHeaderFooter && <Header token={token} />}
       {children}
+      {/* {shouldShowHeaderFooter && <Footer />} */}
     </div>
   );
 }
