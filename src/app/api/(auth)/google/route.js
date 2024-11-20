16cb34f3cc62d1b88d249cd8934c49b93637da8e
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-20 09:29:42 +0500

    Update route.js

--- src/app/api/(auth)/google/route.js
+++ src/app/api/(auth)/google/route.js
@@ -1,45 +1,37 @@
-import { PrismaClient } from "@prisma/client";
 import prisma from "@/lib/prisma";
 
-export default async function POST(req, res) {
+export async function POST(req) {
   function decodeJWT(token) {
     const base64Url = token.split(".")[1];
     const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split("")
-        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
-        .join("")
-    );
-
+    const jsonPayload = Buffer.from(base64, "base64").toString("utf-8");
     return JSON.parse(jsonPayload);
   }
 
   try {
-    const { credential } = req.body;
-    const decodedToken = decodeJWT(credential);
+    const body = await req.json();
+    const { credential } = body;
 
+    const decodedToken = decodeJWT(credential);
     console.log("Decoded token:", decodedToken);
 
     const { email, name } = decodedToken;
 
     let user = await prisma.user.findUnique({
-      where: {
-        email: email,
-      },
+      where: { email },
     });
+
     if (!user) {
       user = await prisma.user.create({
-        data: {
-          email,
-          name,
-        },
+        data: { email, name },
       });
     }
 
-    return res.status(200).json(user);
+    return new Response(JSON.stringify(user), { status: 200 });
   } catch (error) {
     console.error("Authentication error:", error);
-    return res.status(500).json({ message: "Authentication failed" });
+    return new Response(JSON.stringify({ message: "Authentication failed" }), {
+      status: 500,
+    });
   }
 }
