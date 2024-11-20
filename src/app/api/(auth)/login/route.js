0b517f556fa78a69ef26f354a0e5c14942da4d50
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-20 12:42:34 +0500

    Update route.js

--- src/app/api/(auth)/login/route.js
+++ src/app/api/(auth)/login/route.js
@@ -9,24 +9,23 @@ export async function POST(req) {
     const { email, password } = body;
 
     if (!email || !password) {
+      console.error("Missing fields:", { email, password });
       return NextResponse.json({ message: "Missing fields" }, { status: 400 });
     }
 
+    console.log("Querying user with email:", email);
     const exist = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-      select: {
-        password: true,
-        id: true,
-      },
+      where: { email },
+      select: { password: true, id: true },
     });
+    console.log("Query result:", exist);
 
     if (!exist) {
       return NextResponse.json({ message: "User not found" }, { status: 400 });
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, exist.password);
+    console.log("Password match:", isPasswordCorrect);
 
     if (!isPasswordCorrect) {
       return NextResponse.json(
@@ -37,11 +36,12 @@ export async function POST(req) {
 
     cookies().set("token", exist.id, {
       maxAge: 60 * 60 * 24 * 365,
+      path: "/",
     });
 
     return NextResponse.json({ message: "Login successful" }, { status: 200 });
   } catch (error) {
-    console.log(error);
+    console.error("Error occurred:", error);
     return NextResponse.json({ message: "Internal Error" }, { status: 500 });
   }
 }
