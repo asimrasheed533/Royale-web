cac5abf529fb8daf0f39f07799e4331aa0271444
Asim Rasheed <asimrasheed442@gmail.com>
2024-11-25 10:05:57 +0500

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextPWA from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const withPWA = createNextPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/",
+  sw: "service-worker.js",
+});
+
+export default withPWA(nextConfig);
---
+++ src/app/layout.jsx
@@ -0,0 +1,69 @@
+import "react-toastify/dist/ReactToastify.css";
+import "@/style/global.scss";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import { ToastContainer } from "react-toastify";
+
+import { Plus_Jakarta_Sans } from "next/font/google";
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const APP_NAME = "Royal";
+const APP_DEFAULT_TITLE = "Royal Fast Food";
+const APP_TITLE_TEMPLATE = "%L | Royal Fast Food";
+const APP_DESCRIPTION =
+  "Royel Fast Food is a food delivery app that provides a fast and convenient way to order food from a wide variety of restaurants.";
+export const metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport = {
+  themeColor: "#161c24",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+        <body className={plus_jakarta_sans.className}>
+          {children}
+          <ToastContainer
+            position="bottom-right"
+            theme="dark"
+            autoClose={1500}
+          />
+        </body>
+      </GoogleOAuthProvider>
+    </html>
+  );
+}
