7d5dddb56e1c6f42e4d79694fbc508d9b758a2b2
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-17 06:54:28 +0500

    Update page.tsx

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import "@/style/dashboard.scss";
 import Input from "@/components/Input";
-import { InputFile } from "@/components/InputFile";
+import InputFile from "@/components/InputFile";
 
 export default function ProductAdd() {
   return (
     <>
       <div className="product__container">
-        <InputFile />
-        {/* <Input label="Product Name" type="text" name="name" /> */}
+        <InputFile label="Product Image" onChange={() => {}} value={null} />
+        <Input label="Product Name" type="text" name="name" />
       </div>
     </>
   );
