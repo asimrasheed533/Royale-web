96d7d337061124085b2174ffe7b56b1365deba8a
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-17 07:26:59 +0500

    upload image local

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import "@/style/dashboard.scss";
+
 import Input from "@/components/Input";
 import InputFile from "@/components/InputFile";
+import { useState } from "react";
 
 export default function ProductAdd() {
+  const [files, setFiles] = useState<File[] | null>(null);
   return (
     <>
       <div className="product__container">
-        <InputFile label="Product Image" onChange={() => {}} value={null} />
+        <InputFile label="Product Image" onChange={setFiles} value={files} />
         <Input label="Product Name" type="text" name="name" />
       </div>
     </>
