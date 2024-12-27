9260b9a9f759bf288c8dea56b1792c0adbb39b45
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-27 15:02:52 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Add the external image domain here
-  },
-  // Add other configurations here
-};
-module.exports = {
-  images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["res.cloudinary.com", "avatars.githubusercontent.com"],
   },
 };
+
+module.exports = nextConfig;
 export default nextConfig;
