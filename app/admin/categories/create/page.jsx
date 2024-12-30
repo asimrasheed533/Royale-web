42549369f725fd025444ab806c2c21e31aab94d0
Asim Rasheed <asimrasheed442@gmail.com>
2024-12-30 14:34:31 +0500

    Update page.jsx

--- app/admin/categories/create/page.jsx
+++ app/admin/categories/create/page.jsx
@@ -1,5 +1,30 @@
-import React from "react";
-
+"use client";
+import "@/style/dashboard.scss";
+import Input from "@/components/Input";
+// import PictureInput from "@/components/PictureInput";
+import "@uploadthing/react/styles.css";
+import { UploadButton } from "@/utils/uploadthing";
+import useAction from "@/hooks/useAction";
 export default function AddCategories() {
-  return <div>Add Categories</div>;
+  const [submitting, setSubmitting] = useAction({});
+
+  return (
+    <>
+      <div className="product__container">
+        <div className="input__row">
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              console.log("Files: ", res);
+              alert("Upload Completed");
+            }}
+            // onUploadError={(error: Error) => {
+            //   alert(`ERROR! ${error.message}`);
+            // }}
+          />
+          <Input label="Category Name" type="text" name="name" />
+        </div>
+      </div>
+    </>
+  );
 }
